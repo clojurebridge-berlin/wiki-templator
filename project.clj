d2277f14ee6f9d3bbf0cae702fa635b8e7976bad
Arne Brasseur <arne@arnebrasseur.net>
2016-05-05 13:17:03 +0200

    draw the rest of the owl

--- project.clj
+++ project.clj
@@ -26,7 +26,8 @@
                  [conman "0.5.1"]
                  [org.postgresql/postgresql "9.4-1206-jdbc4"]
                  [org.webjars/webjars-locator-jboss-vfs "0.1.0"]
-                 [luminus-immutant "0.2.0"]]
+                 [luminus-immutant "0.2.0"]
+                 [clj-http "3.0.1"]]
 
   :min-lein-version "2.0.0"
 
@@ -42,7 +43,7 @@
   :target-path "target/%s/"
   :profiles
   {:uberjar {:omit-source true
-             
+
              :aot :all
              :uberjar-name "templator.jar"
              :source-paths ["env/prod/clj"]
@@ -54,8 +55,8 @@
                                  [ring/ring-devel "1.4.0"]
                                  [pjstadig/humane-test-output "0.8.0"]]
                   :plugins      [[com.jakemccrary/lein-test-refresh "0.14.0"]]
-                  
-                  
+
+
                   :source-paths ["env/dev/clj" "test/clj"]
                   :resource-paths ["env/dev/resources"]
                   :repl-options {:init-ns user}
--- src/clj/templator/routes/home.clj
+++ src/clj/templator/routes/home.clj
@@ -1,17 +1,44 @@
 (ns templator.routes.home
-  (:require [templator.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
-            [ring.util.http-response :as response]
-            [clojure.java.io :as io]))
+  (:require [clj-http.client :as client]
+            [clojure.string :as str]
+            [compojure.core :refer [defroutes GET POST]]
+            [templator.layout :as layout]))
+
+(defn raw-md-url [wiki-url]
+  (let [[scheme _ _ team repo _ page] (str/split wiki-url #"/")]
+    (str scheme "//raw.githubusercontent.com/wiki/" team "/" repo "/" page ".md")))
 
 (defn home-page []
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html"))
 
-(defn about-page []
-  (layout/render "about.html"))
+(defn fill-in-page [wiki-link placeholders]
+  (layout/render
+   "fill-in.html" {:wiki-link wiki-link :placeholders placeholders}))
+
+(defn result-page [md]
+  (layout/render
+   "result.html" {:md md}))
+
+(defn fetch-md [wiki-link]
+  (-> wiki-link
+      raw-md-url
+      client/get
+      :body))
+
+(defn find-placeholders [md]
+  (sort (distinct (map last (re-seq #"\{\{([^\}]*)\}\}" md)))))
+
 
 (defroutes home-routes
   (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
-
+  (POST "/fill-in" [wiki-link]
+    (let [placeholders (-> wiki-link fetch-md find-placeholders)]
+      (fill-in-page wiki-link placeholders)))
+  (POST "/result" {{:keys [wiki-link] :as params} :params}
+    (let [md (fetch-md wiki-link)
+          placeholders (find-placeholders md)]
+      (result-page
+       (reduce (fn [md ph]
+                 (str/replace md (str "{{" ph "}}") (get params (keyword ph))))
+               md placeholders)))))
